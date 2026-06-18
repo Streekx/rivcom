c698f176d0e4eab5abf0783eb81516f34608af4c
Streekx <anshkumarchan@gmail.com>
2026-06-18 09:30:07 +0530

    Define TokenType enum and Token struct in Token.hpp

--- include/Token.hpp
+++ include/Token.hpp
@@ -1,1 +1,36 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
 
+#include <string>
+
+enum class TokenType {
+
+   RIVEN,
+   MAIN,
+
+   LEFT_PAREN,
+  RIGHT_PAREN,
+
+  LEFT_BRACE,
+RIGHT_BRACE,
+
+IMPR,
+OUTPUT_OP,
+STRING,
+SEMICOLON,
+EOF,
+
+
+};
+
+
+struct TokenType {
+TokenType Token;
+
+std::string Text;
+
+
+
+};
+
+#endif
